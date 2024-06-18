8f9cb9007186b8375b848e6f757ff546eaa59b3f
Leo-Lepin <129879137+Leo-Lepin@users.noreply.github.com>
2024-06-18 15:01:46 +0300

    visual

--- Assets/Scripts/ObjectVisualization/TurretVisualizer.cs
+++ Assets/Scripts/ObjectVisualization/TurretVisualizer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class TurretVisualizer : MonoBehaviour
 {
-    [SerializeField] private float adjusmentRotation; //для этой модельки 90
+    [SerializeField] private float adjusmentRotation; //для этой модельки 85
     private void Update()
     {
         if (Input.GetMouseButtonUp(0))
@@ -15,9 +15,19 @@ public class TurretVisualizer : MonoBehaviour
             if (Physics.Raycast(ray, out hit, Mathf.Infinity))
             {
                 GameObject click = hit.collider.gameObject;
+                Point coordClick = new Point((int)click.transform.position.x, (int)click.transform.position.z);
+                Point coodinate = CurrentPlayer.LastPurchasedTurret.Coodinate;
+                if (coordClick.GetDistSquared(coodinate) != 1)
+                    return;
+                
+                CurrentPlayer.LastPurchasedTurret.SetDirection(coordClick);
+                Player player = PlayersContainer.Players[CurrentPlayer.CurrentPlayerNumber];
+                player.BuyObject(CurrentPlayer.LastPurchasedTurret, coodinate);
+                CurrentPlayer.LastPurchasedTurret = null;
                 float y_angle = Vector3.Angle(Vector3.forward, click.transform.position-transform.position)+adjusmentRotation;
                 transform.rotation = Quaternion.Euler(-90, y_angle, 0);
                 enabled = false;
+
             }
         }
     }
