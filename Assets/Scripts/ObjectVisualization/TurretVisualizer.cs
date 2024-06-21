04e09a452c72b24148fef22dcb5050752a2dfb1f
Leo-Lepin <129879137+Leo-Lepin@users.noreply.github.com>
2024-06-21 11:42:57 +0300

    Дебаг

--- Assets/Scripts/ObjectVisualization/TurretVisualizer.cs
+++ Assets/Scripts/ObjectVisualization/TurretVisualizer.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class TurretVisualizer : MonoBehaviour
 {
-    [SerializeField] private float adjusmentRotation; //для этой модельки 85
+
+    private int _currentPlayerNumber = CurrentPlayer.CurrentPlayerNumber;
+
+    private void Start()
+    {
+        Raycaster.ChangeEnabled();
+    }
     private void Update()
     {
         if (Input.GetMouseButtonUp(0))
@@ -16,16 +22,40 @@ public class TurretVisualizer : MonoBehaviour
             {
                 GameObject click = hit.collider.gameObject;
                 Point coordClick = new Point((int)click.transform.position.x, (int)click.transform.position.z);
-                Point coodinate = CurrentPlayer.LastPurchasedTurret.Coodinate;
-                if (coordClick.GetDistSquared(coodinate) != 1)
+                Point coordinate = CurrentPlayer.LastPurchasedTurret.Coordinate;
+                if (coordClick.GetDistSquared(coordinate) != 1)
                     return;
-                
+
                 CurrentPlayer.LastPurchasedTurret.SetDirection(coordClick);
+
+
+
+                if (CurrentPlayer.CurrentPlayerNumber != _currentPlayerNumber)
+                {
+                    CurrentPlayer.NextPlayer();
+                }
                 Player player = PlayersContainer.Players[CurrentPlayer.CurrentPlayerNumber];
-                player.BuyObject(CurrentPlayer.LastPurchasedTurret, coodinate);
+                player.BuyObject(CurrentPlayer.LastPurchasedTurret, coordinate);
                 CurrentPlayer.LastPurchasedTurret = null;
-                float y_angle = Vector3.Angle(Vector3.forward, click.transform.position-transform.position)+adjusmentRotation;
+                float y_angle = 0;
+                Point dist = coordClick - coordinate;
+                if (dist == new Point(0, 1))
+                {
+                    y_angle = 90f;
+                }
+                else if (dist == new Point(1, 0))
+                {
+                    y_angle = 180f;
+                }
+                else if (dist == new Point(0, -1))
+                {
+                    y_angle = 270f;
+                }
                 transform.rotation = Quaternion.Euler(-90, y_angle, 0);
+
+                CurrentPlayer.NextPlayer();
+
+                Raycaster.ChangeEnabled();
                 enabled = false;
 
             }
